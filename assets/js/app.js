489922f564b802c1170dbc6574a7f286f0e3306e
tristanchausson <tristanchausson@me.com>
2017-12-22 13:19:31 +0100

    adding tasks to the list and creating the checkboxes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,33 @@
 //alert("ToDo List !");
+var bouton = document.getElementById("add");
+var box = document.getElementById("task");
+var n = 1;
+
+bouton.addEventListener("click", function(){
+        console.log("hello!")
+    var content = document.getElementById("textarea1").value;
+        console.log(content);
+    var li = document.createElement("li");
+        console.log(li);
+    var check = document.createElement("input");
+        check.setAttribute("type", "checkbox");
+        check.setAttribute("id", "test"+n);
+
+    li.append(check);
+
+    var label = document.createElement("label");
+        label.setAttribute("for", "test"+n);
+        label.append(content);
+            n++;
+
+li.appendChild(label);
+
+box.appendChild(li); 
+
+console.log(n);
+
+
+});
+
 
 
